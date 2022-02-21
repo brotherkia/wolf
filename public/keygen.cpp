1d710ddf42f18519314a2449438bdf3b9a858986
brotherkia <73574047+brotherkia@users.noreply.github.com>
2022-02-21 12:05:24 +0330

    Update keygen.cpp

--- public/keygen.cpp
+++ public/keygen.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main()
 {
-    unsigned int reg =0, c=0 , reg2 = 0 ,sum = 0 , x=0 , y=0 , password[100];
-    reg = 'A';
-    char hex1[20] , hex2[20] , hex3[20];
-    // string user = "A";
-    // cout<<"Enter username: ";
-    // cin>>user;
-    // for(int i=0; i <= user.length();i++){
-    //     c = user[i];
-    //     sum = c*(user.length()-i);
-    //     reg += sum;
-    // }
+    unsigned int reg =0, c=0 , reg2 = 0 ,sum = 0 , x=0 , y=0 , password[8];
+    char hex1[20] , hex2[20] ;
+    string user;
+    cout<<"Enter username: ";
+    cin>>user;
+    for(int i=0; i <= user.length();i++){
+        c = user[i];
+        sum = c*(user.length()-i);
+        reg += sum;
+    }
     reg = reg ^ 0x13131313;
     reg = ~reg ;
     reg = reg ^ 0x1234ABCD;
@@ -23,9 +22,9 @@ int main()
     reg2 = reg2 & 0xF0F0F0F0;
     reg2 = reg2 >> 4;
     
-    sprintf(hex1, "%X", reg);
-    sprintf(hex2,"%X", reg2);
-    cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
+    // sprintf(hex1, "%X", reg);
+    // sprintf(hex2,"%X", reg2);
+    // cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
 
 
 
@@ -44,52 +43,32 @@ int main()
     
     
     for (int i=0 ; i<4 ; i++){
-        x = reg % 100 ;
-        // printf(hex3 ,"%X",x);
-        y = reg % 100 ; 
+        x = reg % 0x100 ;
+        y = reg2 % 0x100 ; 
         if (x < 0x9)
             x = x |0x30;
         else
             x += 0x37;
         if (y < 0x9 )
-            y = y | 30 ;
+            y = y | 0x30 ;
         else
             y += 0x37;
-        reg /= 100;
-        reg2 /= 100;
-        cout<<"---";
-        cout<<"\n";
+        reg /= 0x100;
+        reg2 /= 0x100;
         password[i] = x;
         password[i+4]= y;
+        // cout<<std::hex<<y<<"\n";
+        // cout<<"\n--------\n"<<char(y);
     }
-    cout<<std::hex<<password[i]
    
+//   for(int i=4 ; i<9 ; i++)
+//     cout<<char(password[i]);
    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    
-    // reg2 = 0x30303030 | reg2 ;
-    // reg = reg | 0x37303030;
-    // sprintf(hex1, "%X", reg);
-    // sprintf(hex2,"%X", reg2);
-    // cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
-    // reg =  0x37303030 | reg ;
-    // // cout<<reg<<"\n"<<reg2;
-    // sprintf(hex1, "%X", reg);
-    // sprintf(hex2,"%X", reg2);
-    // cout<<hex1<<"\n"<<hex2;
+    cout<<"Your password is : 12-";
+    for(int i = 0 ; i<8 ; i++){
+        cout<<char(password[i]);
+        if(i==3)
+            cout<<"-";
+    }
     return 0;
 }
