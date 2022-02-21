bcc89fcdd5f0a39e307c37e7c78ae472d720f751
brotherkia <73574047+brotherkia@users.noreply.github.com>
2022-02-21 11:05:00 +0330

    Update keygen.cpp

--- public/keygen.cpp
+++ public/keygen.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
-    unsigned int reg =0, c=0 , reg2 = 0 ,sum = 0;
+    unsigned int reg =0, c=0 , reg2 = 0 ,sum = 0 , x=0 , y=0 , password[100];
     reg = 'A';
-    char hex1[20] , hex2[20];
+    char hex1[20] , hex2[20] , hex3[20];
     // string user = "A";
     // cout<<"Enter username: ";
     // cin>>user;
@@ -26,17 +26,70 @@ int main()
     sprintf(hex1, "%X", reg);
     sprintf(hex2,"%X", reg2);
     cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
+
+
+
+
+
+    // x = (reg % 100) | 0x30;
+    // cout<<std::hex<<x<<"||||||||||||";
+    // sprintf(hex3 , "%X",x);
+    // cout<<hex3<<"\n";
     
     
-    reg2 = 0x30303030 | reg2 ;
-    reg = reg | 0x37303030;
-    sprintf(hex1, "%X", reg);
-    sprintf(hex2,"%X", reg2);
-    cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
-    reg =  0x37303030 | reg ;
-    // cout<<reg<<"\n"<<reg2;
-    sprintf(hex1, "%X", reg);
-    sprintf(hex2,"%X", reg2);
-    cout<<hex1<<"\n"<<hex2;
-    // return 0;
+    
+    
+    
+    
+    
+    
+    for (int i=0 ; i<4 ; i++){
+        x = reg % 100 ;
+        // printf(hex3 ,"%X",x);
+        y = reg % 100 ; 
+        if (x < 0x9)
+            x = x |0x30;
+        else
+            x += 0x37;
+        if (y < 0x9 )
+            y = y | 30 ;
+        else
+            y += 0x37;
+        reg /= 100;
+        reg2 /= 100;
+        cout<<"---";
+        cout<<"\n";
+        password[i] = x;
+        password[i+4]= y;
+    }
+    cout<<std::hex<<password[i]
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    // reg2 = 0x30303030 | reg2 ;
+    // reg = reg | 0x37303030;
+    // sprintf(hex1, "%X", reg);
+    // sprintf(hex2,"%X", reg2);
+    // cout<<hex1<<"\n"<<hex2 <<"\n-----------\n";
+    // reg =  0x37303030 | reg ;
+    // // cout<<reg<<"\n"<<reg2;
+    // sprintf(hex1, "%X", reg);
+    // sprintf(hex2,"%X", reg2);
+    // cout<<hex1<<"\n"<<hex2;
+    return 0;
 }
